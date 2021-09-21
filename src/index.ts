f3ecf7c1166b285dc6449acef1f98f20feb280de
Maurice Dalderup <10922045+DalderupMaurice@users.noreply.github.com>
2021-09-21 19:36:42 +0200

    initial discord stuff

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-(() => {
-  console.log(Math.PI);
-  console.log(Math.PI);
-  console.log(Math.PI);
-  console.log(Math.PI);
-})();
+import { Client, Intents } from "discord.js";
+import consola from "consola";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+  consola.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login("token");
